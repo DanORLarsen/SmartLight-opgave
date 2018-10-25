fb6302bcc6e352a7673a3c973665db6eda60d0a8
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2018-10-25 11:01:52 +0200

    Add files via upload

--- src/hu/hungarian/Main.java
+++ src/hu/hungarian/Main.java
@@ -10,5 +10,16 @@ public class Main {
 	    // write your code here
         SmartLight s = new SmartLight();
         System.out.println(s.toString());
+
+        SmartLight dan1 = new SmartLight();
+        dan1.setColor(Color.PINK);
+        dan1.setLightPercent((short)99);
+        System.out.println(dan1.getColor());
+        System.out.println(s.toString());
+
+        SmartLight dan2 = new SmartLight();
+        dan2.setLightPercent((short)26);
+        dan2.setColor(Color.BLACK);
+        System.out.println(s.toString());
     }
 }
